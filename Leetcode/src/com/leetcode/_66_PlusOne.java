7907bc3ecc34d9917a9d5825c5d32c79ebd33934
halolong <halolong@hotmail.com>
2019-04-03 19:56:55 +0800

    Create _66_PlusOne.java
    Math. Finish (2): LeetCode No.66 Plus One

---
+++ Leetcode/src/com/leetcode/_66_PlusOne.java
@@ -0,0 +1,30 @@
+class _66_PlusOne {
+
+     /**
+     * 66. Plus One
+     * When: 2019/04/03
+     */
+    public int[] plusOne(int[] digits) {
+        /**
+        * 考虑三种情况：
+        * case 1: 1011 -> 1012
+        * case 2: 1099 -> 1100
+        * case 3: 9999 -> 10000 (这里需要新开辟空间，原有的长度+1即可)
+        *
+        */
+        if (digits == null || digits.length == 0) return digits;
+        
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            } else {
+                digits[i] = 0;
+            }
+        }
+        // 第三种情况
+        int[] res = new int[digits.length + 1];
+        res[0] = 1;
+        return res;
+    }
+}
