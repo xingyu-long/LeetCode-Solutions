f115c056ff91cbe4486f5f2af5c48a1708b0c9e4
halolong <halolong@hotmail.com>
2019-04-30 15:06:23 +0800

    2019/04/30 backtracking. Finish ( 3 ) : LeetCode No.31 Next permutation 46. permutations 47. permutations II

---
+++ Leetcode/src/com/leetcode/backtracking/_31_NextPermutation.java
@@ -0,0 +1,71 @@
+package com.leetcode.backtracking;
+
+public class _31_NextPermutation {
+
+    /**
+     * 31. Next Permutation
+     * When: 2019/04/30
+     *
+     * solution:
+     * 首先找到第一个小于后一个数的位置。
+     * 然后这个位置如果不能找到，就把当前的进行翻转
+     * 再寻找大于前面那个的第一个数
+     * 交换
+     * 然后从firstSmall+1 -> end 翻转
+     *
+     * test case:
+     *    1, 2, 7, 3, 4, 1
+     *             ^
+     *    1, 2, 7, 3, 4, 1
+     *             ^  ^
+     *    1, 2, 7, 4, 3, 1
+     *
+     *    1, 2, 7, 4, 1, 3
+     *
+     *
+     *    
+     * @param nums
+     */
+    public void nextPermutation(int[] nums) {
+        if (nums == null || nums.length == 0) return;
+
+        int firstSmall = -1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                firstSmall = i;
+                break;
+            }
+        }
+
+        // 就表明这个是最大的情况，所以直接反转最小即可
+        if (firstSmall == -1) {
+            reverse(nums, 0, nums.length - 1);
+            return;
+        }
+
+        int firstLarge = -1;
+        for (int i = nums.length - 1; i > firstSmall; i--) {
+            if (nums[i] > nums[firstSmall]) {
+                firstLarge = i;
+                break;
+            }
+        }
+
+        swap(nums, firstSmall, firstLarge);
+        reverse(nums, firstSmall + 1, nums.length - 1);
+        return;
+    }
+
+
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public void reverse(int[] nums, int i, int j) {
+        while(i < j) {
+            swap(nums, i++, j--);
+        }
+    }
+}
---
+++ Leetcode/src/com/leetcode/backtracking/_46_Permutations.java
@@ -0,0 +1,41 @@
+package com.leetcode.backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _46_Permutations {
+
+    /**
+     * 46. Permutations
+     * When: 2019/04/30
+     *
+     * solution:
+     * 基本思路与前面一致 但是没有start这个参数，因为这次是全部在数组里面获得并且 i 只能从 0开始 这样才能获取全部
+     *
+     * space: O(n)
+     * time: O(n * n!)
+     *
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length == 0) return res;
+        helper(res, new ArrayList<Integer>(), nums);
+        return res;
+    }
+
+    public void helper(List<List<Integer>> res, List<Integer> list, int[] nums) {
+        if (list.size() == nums.length) {
+            res.add(new ArrayList<>(list));
+            return;
+        }
+        // 这里一直从0开始，因为这里需要能够遍历完里面的元素才行，以前的方法就不行
+        for (int i = 0; i < nums.length; i++) {
+            if (list.contains(nums[i])) continue;  // O(n)
+            list.add(nums[i]);
+            helper(res, list, nums);
+            list.remove(list.size() - 1);
+        }
+    }
+}
