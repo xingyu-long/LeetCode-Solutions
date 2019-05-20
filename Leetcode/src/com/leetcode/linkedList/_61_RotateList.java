03f11bd9c2d1062916adf92e0303a55e86d3cb89
halolong <halolong@hotmail.com>
2019-05-20 10:03:53 +0800

    2019/05/20 linkedList. Finish ( 2 ) : LeetCode No.61 rotate list No.86 partition list

---
+++ Leetcode/src/com/leetcode/linkedList/_61_RotateList.java
@@ -0,0 +1,39 @@
+package com.leetcode.linkedList;
+
+import com.leetcode.lib.ListNode;
+
+public class _61_RotateList {
+
+    /**
+     * 61. Rotate List
+     * When: 2019/05/20
+     *
+     * For example:
+     *   Given 1->2->3->4->5->NULL and k = 2,
+     *   return 4->5->1->2->3->NULL.
+     *
+     * solution:
+     * 首先计数，并且将链表连起来，然后走到合适位置 断开
+     *  @param head
+     * @param k
+     * @return
+     */
+    //time: O(n) space: O(1)
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null) return head;
+        ListNode index = head; // 用来计数时使用
+        int len = 1;
+        while (index.next != null) { //这样的话 最后index在最后一个元素
+            index = index.next;
+            len++;
+        }
+        index.next = head;
+        // 尽管是要走到len - k % len 这个位置 但只需要 前面-1 这么多步而已
+        for (int i = 1; i < len - k % len; i++) {
+            head = head.next;
+        }
+        ListNode res = head.next;
+        head.next = null;
+        return res;
+    }
+}
---
+++ Leetcode/src/com/leetcode/linkedList/_86_PartitionList.java
@@ -0,0 +1,48 @@
+package com.leetcode.linkedList;
+
+import com.leetcode.lib.ListNode;
+
+public class _86_PartitionList {
+    /**
+     *  86. Partition List
+     *  When: 2019/05/20
+     * Given a linked list and a value x, partition it such that all nodes less than x come before nodes
+     * greater than or equal to x.
+     *
+     * You should preserve the original relative order of the nodes in each of the two partitions.
+     *
+     * For example,
+     *  Given 1->4->3->2->5->2 and x = 3,
+     *  return 1->2->2->4->3->5.
+     *
+     *  solution:
+     *  新建两个左右的点，然后记录其比它小和比它大的情况
+     *  然后两个表连起来（这里要注意需要用curRight/left来移动
+     *
+     * @param head
+     * @param x
+     * @return
+     */
+    public ListNode partition(ListNode head, int x) {
+        if (head == null || head.next == null) return head;
+        // 分成左右
+        ListNode dummyLeft = new ListNode(0);
+        ListNode dummyRight = new ListNode(0);
+        // curXXX用来移动
+        ListNode curLeft = dummyLeft;
+        ListNode curRight = dummyRight;
+        while (head != null) {
+            if (head.val < x) {
+                curLeft.next = head;
+                curLeft = curLeft.next;
+            } else {
+                curRight.next = head;
+                curRight = curRight.next;
+            }
+            head = head.next;
+        }
+        curRight.next = null;
+        curLeft.next = dummyRight.next;
+        return dummyLeft.next;
+    }
+}
