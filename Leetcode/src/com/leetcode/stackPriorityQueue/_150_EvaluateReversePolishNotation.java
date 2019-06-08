b19c08dc1d7549a537dcb283aee169c66177913f
longxingyu <longxingyu@halolongs-MacBook-Air.local>
2019-06-08 19:23:39 +0800

    2019/06/08 stack, queue & pq. Finish ( +2 ) : No.71 simplify path No.150 evaluate reverse polish notation

---
+++ Leetcode/src/com/leetcode/stackPriorityQueue/_150_EvaluateReversePolishNotation.java
@@ -0,0 +1,36 @@
+package com.leetcode.stackPriorityQueue;
+
+import java.util.Stack;
+
+public class _150_EvaluateReversePolishNotation {
+
+    /**
+     *  150. Evaluate Reverse Polish Notation
+        When: 2019/06/08
+        solution: 使用stack存储数字，然后遇到符号就pop进行运算。
+     * @param tokens
+     * @return
+     */
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < tokens.length; i++) {
+            if (tokens[i].equals("+")) {
+                stack.push(stack.pop() + stack.pop());
+            } else if (tokens[i].equals("-")) {
+                int in1 = stack.pop();
+                int in2 = stack.pop();
+                stack.push(in2 - in1);
+            } else if (tokens[i].equals("*")) {
+                stack.push(stack.pop() * stack.pop());
+            } else if (tokens[i].equals("/")) {
+                int in1 = stack.pop();
+                int in2 = stack.pop();
+                stack.push(in2 / in1);
+            } else {
+                // 这里就是数字
+                stack.push(Integer.parseInt(tokens[i]));
+            }
+        }
+        return stack.pop();
+    }
+}
---
+++ Leetcode/src/com/leetcode/stackPriorityQueue/_71_SimplifyPath.java
@@ -0,0 +1,37 @@
+package com.leetcode.stackPriorityQueue;
+
+import java.util.Stack;
+
+public class _71_SimplifyPath {
+
+    /**
+     *  71. Simplify Path
+        When: 2019/06/08
+
+        solution: 使用stack来保存其字符 遇到.. 就返回上一层 遇到 . 和空白就不管；遇到字符就push
+        最后拼接在一起
+     * @param path
+     * @return
+     */
+    public String simplifyPath(String path) {
+        Stack<String> stack = new Stack<>();
+        String[] paths = path.split("/+");
+        for (String s : paths) {
+            if (s.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else if (!s.equals(".") && !s.equals("")) {
+                stack.push(s);
+            }
+        }
+        String res = "";
+        while (!stack.isEmpty()) {
+            res = "/" + stack.pop() + res;
+        }
+        if (res.length() == 0) {
+            return "/";
+        }
+        return res;
+    }
+}
