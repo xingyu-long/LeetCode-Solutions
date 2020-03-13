b1476f8771e87e47b2fc83000254aa5260ac42e6
XingyuLong <halolong@hotmail.com>
2020-03-12 22:12:10 -0400

    03/11/2020, total:1, DP(stock)

--- Leetcode/src/com/leetcode/dynamicProgramming/_123_BestTimetoBuyandSellStockIII.java
+++ Leetcode/src/com/leetcode/dynamicProgramming/_123_BestTimetoBuyandSellStockIII.java
@@ -27,4 +27,20 @@ public class _123_BestTimetoBuyandSellStockIII {
         }
         return dp[t][days - 1];
     }
+
+    // O(M*N) space:O(M*N)
+    public int maxProfit2(int[] prices) {
+        if (prices == null || prices.length == 0) return 0;
+        int m = 2;
+        int n = prices.length;
+        int[][] dp = new int[m + 1][n];
+        for (int i = 1; i <= m; i++) {
+            int maxDiff = -prices[0];
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = Math.max(dp[i][j - 1], prices[j] + maxDiff);
+                maxDiff = Math.max(maxDiff, dp[i - 1][j] - prices[j]);
+            }
+        }
+        return dp[m][n - 1];
+    }
 }
