59a107306fe7f554b99a602c697b34611669aae1
XingyuLong <halolong@hotmail.com>
2020-07-14 22:43:53 -0400

    07/14/2020 total: 3

--- Leetcode/src/com/leetcode/math/_1344_AngleBetweenHandsofaClock.java
+++ Leetcode/src/com/leetcode/math/_1344_AngleBetweenHandsofaClock.java
@@ -1,7 +1,11 @@
 package com.leetcode.math;
 
+/**
+ * @Date: 07/14/2020
+ * @Description: Math
+ **/
 public class _1344_AngleBetweenHandsofaClock {
-    // 1344. Angle Between Hands of a Clock
+
     // 相当于以钟的12点位置作为起始点。
     public double angleClock(int hour, int minutes) {
         double dh = hour;
@@ -12,7 +16,9 @@ public class _1344_AngleBetweenHandsofaClock {
         hangle %= 360;
 
         double mangle = dm / 60 * 360;
-        if (mangle > hangle) return Math.min(mangle - hangle, 360 - (mangle - hangle));
+        if (mangle > hangle) {
+            return Math.min(mangle - hangle, 360 - (mangle - hangle));
+        }
         return Math.min(hangle - mangle, 360 - (hangle - mangle));
     }
 }
