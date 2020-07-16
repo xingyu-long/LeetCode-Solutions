519813db562440cd130bdac9870d56776d1762c4
XingyuLong <halolong@hotmail.com>
2020-07-16 09:03:31 -0400

    07/16/2020, total: 9

--- Leetcode/src/com/leetcode/string/_541_ReverseStringII.java
+++ Leetcode/src/com/leetcode/string/_541_ReverseStringII.java
@@ -1,22 +1,35 @@
 package com.leetcode.string;
 
+/**
+ * @Date: 07/15/2020
+ * @Description: String, reverse
+ **/
 public class _541_ReverseStringII {
+
     public String reverseStr(String s, int k) {
-        // 转为char arr
-        if (s == null || s.length() == 0) return "";
-        if (k == 0) return s;
         char[] chs = s.toCharArray();
-        int n = s.length();
-        int i = 0;
-        while (i < n) {
-            int j = Math.min(i + k - 1, n - 1); // 相当于每次判断走了k的位置，与末尾比较
-            exch(chs, i, j);
-            i += 2 * k;
+        int left = s.length();
+        int n = left, index = 0;
+        while (index < n) {
+            if (left >= 2 * k) {
+                reverse(chs, index, index + k);
+                index += 2 * k;
+                left -= 2 * k;
+            } else if (left >= k) {
+                reverse(chs, index, index + k);
+                index += 2 * k;
+                left -= 2 * k;
+            } else if (left < k) {
+                reverse(chs, index, index + left);
+                index += left;
+                left = 0;
+            }
         }
         return new String(chs);
     }
 
-    public void exch(char[] chs, int left, int right) {
+    private void reverse(char[] chs, int left, int right) {
+        right -= 1;
         while (left < right) {
             char temp = chs[left];
             chs[left] = chs[right];
