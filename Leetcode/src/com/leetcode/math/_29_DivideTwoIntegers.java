4301085eda568ad5eb2c6239a5dff51ed6a92541
XingyuLong <halolong@hotmail.com>
2020-07-16 22:37:23 -0400

    07/16/2020, total: 5

--- Leetcode/src/com/leetcode/math/_29_DivideTwoIntegers.java
+++ Leetcode/src/com/leetcode/math/_29_DivideTwoIntegers.java
@@ -1,43 +1,36 @@
 package com.leetcode.math;
 
+/**
+ * @Date: 2019/04/04, 2019/8/8, 07/16/2020
+ * @Description: Math
+ **/
 public class _29_DivideTwoIntegers {
 
-    /**
-     * 29. Divide Two Integers
-     * When: 2019/04/04
-     * review1:2019/8/8
-     *
-     * Test case:
-     * dividend = 10, divisor = 3 -> res = 3
-     * sign = 1
-     *      sum = 3, multiple = 1; 执行while sum = 3 + 3 = 6 multiple = 2; return 2 + divide(10 - 6 = 4, 3)
-     *                                                                                sum = 3, multiple = 1; return 1+ divide(4 - 3 = 1, 3)
-     *                                                                                                                 1 < 3 return 0;
-     *     return 0 + 1 + 2 = 3
-     *
-     * @param dividend
-     * @param divisor
-     * @return
-     */
     public int divide(int dividend, int divisor) {
         // 考虑三种极端情况 1. + - 号 2. 越界问题 3. 被除数为0或者被除数小于除数
         int sign = 1;
-        if ((dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0)) sign = -1;
-        long ldividend = Math.abs((long)dividend);
-        long ldivisor = Math.abs((long)divisor);
-        if (ldividend < ldivisor || ldividend == 0) return 0;
-        long lres = divide(ldividend, ldivisor);
-        int res;
-        if (lres > Integer.MAX_VALUE) {
-            res = (sign == 1) ? Integer.MAX_VALUE: Integer.MIN_VALUE;
-        } else {
-            res = (int)lres * sign;
+        if ((dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0)) {
+            sign = -1;
         }
-        return res;
+        long ldividend = Math.abs((long) dividend);
+        long ldivisor = Math.abs((long) divisor);
+        if (ldividend < ldivisor || ldividend == 0) {
+            return 0;
+        }
+        long lres = divide(ldividend, ldivisor);
+        if ((sign == -1) && lres * sign < Integer.MIN_VALUE) {
+            return Integer.MIN_VALUE;
+        }
+        if ((sign == 1) && lres * sign > Integer.MAX_VALUE) {
+            return Integer.MAX_VALUE;
+        }
+        return sign * (int) lres;
     }
 
     public long divide(long ldividend, long ldivisor) {
-        if (ldividend < ldivisor) return 0;
+        if (ldividend < ldivisor) {
+            return 0;
+        }
         long sum = ldivisor;
         long multiple = 1;
         while ((sum + sum) < ldividend) {
