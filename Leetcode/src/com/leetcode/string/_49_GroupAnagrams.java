e4be215e78662adaadcce418435ae970ad0dbcbd
Clark Long <halolong@hotmail.com>
2021-08-12 09:44:13 -0700

    2021-08-12-09:44:13 Changes were made in following file(s): 49.Group Anagrams

--- Leetcode/src/com/leetcode/string/_49_GroupAnagrams.java
+++ Leetcode/src/com/leetcode/string/_49_GroupAnagrams.java
@@ -1,3 +1,8 @@
+/*
+ * @Date: 08/11/2020 15:07:14
+ * @LastEditTime: 08/12/2021 09:44:06
+ * @Description: HashMap, Counting sort
+ */
 package com.leetcode.string;
 
 import java.util.ArrayList;
@@ -7,22 +12,13 @@ import java.util.List;
 
 public class _49_GroupAnagrams {
 
-    /**
-     * 49. Group Anagrams
-     * When: 2019/03/28, 2019/7/21, 11/7/2019
-     * 04/06/2020
-     * 涉及到的数据结构：
-     * ArrayList<>(), Arrays.sort(), map.containsKey(),list.add
-     *
-     * @param strs
-     * @return
-     */
-    // solution1: 使用sort来进行操作  time: O(n * nlogn) space:O(n)
+    // solution1: 使用sort来进行操作 time: O(n * nlogn) space:O(n)
     // 使用HashMap进行记录其排完序的str（数目肯定比strs少）key: str value: res.size()（这样方便记录）
     // 这种方法还不如直接最后添加呢
     public List<List<String>> groupAnagrams(String[] strs) {
         List<List<String>> res = new ArrayList<>();
-        if (strs == null || strs.length == 0) return res;
+        if (strs == null || strs.length == 0)
+            return res;
         HashMap<String, Integer> map = new HashMap<>();
 
         for (String str : strs) {
@@ -34,7 +30,8 @@ public class _49_GroupAnagrams {
                 List<String> list = res.get(map.get(sortStr));
                 list.add(str);
             } else {
-                // 一开始没有相同的元素下 开始的话 首先会创建一个list（用来存放新的array）然后将map作为标记（这里用size做index 十分巧妙）然后使用res.add(list) 即加入
+                // 一开始没有相同的元素下 开始的话 首先会创建一个list（用来存放新的array）然后将map作为标记（这里用size做index
+                // 十分巧妙）然后使用res.add(list) 即加入
                 List<String> list = new ArrayList<>();
                 list.add(str);
                 map.put(sortStr, res.size());
@@ -44,12 +41,12 @@ public class _49_GroupAnagrams {
         return res;
     }
 
-
     // counting sort 的思想 利用跟字符的相差来记录每个字符的个数并且进行类似排序操作
     // time: O(maxLen * n) space: O(n)
     public List<List<String>> groupAnagrams2(String[] strs) {
         List<List<String>> res = new ArrayList<>();
-        if (strs == null || strs.length == 0) return res;
+        if (strs == null || strs.length == 0)
+            return res;
         HashMap<String, List<String>> map = new HashMap<>();
         for (String str : strs) {
             int[] count = new int[26];
@@ -61,7 +58,7 @@ public class _49_GroupAnagrams {
                 // 这里没有成功转为字符
                 if (count[i] != 0) {
                     sb.append(count[i]);
-                    sb.append((char)(i + 'a')); // count[i]和当前两个连在一起写的话，会以为是在相加，中间应该+""+
+                    sb.append((char) (i + 'a')); // count[i]和当前两个连在一起写的话，会以为是在相加，中间应该+""+
                 }
             }
             // char[] chs = str.toCharArray();
