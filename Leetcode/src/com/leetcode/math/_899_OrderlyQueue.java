d360bd7cdabe3e6e905c679ab2fb4a8e022830eb
Clark Long <halolong@hotmail.com>
2021-09-05 12:15:17 -0700

    2021-09-05-12:15:17 Changes were made in following file(s): 899.Orderly Queue

--- Leetcode/src/com/leetcode/math/_899_OrderlyQueue.java
+++ Leetcode/src/com/leetcode/math/_899_OrderlyQueue.java
@@ -1,6 +1,6 @@
 /*
  * @Date: 09/05/2021 11:57:29
- * @LastEditTime: 09/05/2021 12:10:42
+ * @LastEditTime: 09/05/2021 12:15:11
  * @Description: Math
  */
 package com.leetcode.math;
@@ -8,7 +8,6 @@ package com.leetcode.math;
 import java.util.Arrays;
 
 // time: O(n^2)
-// test
 public class _899_OrderlyQueue {
     public String orderlyQueue(String s, int k) {
         if (k > 1) {
