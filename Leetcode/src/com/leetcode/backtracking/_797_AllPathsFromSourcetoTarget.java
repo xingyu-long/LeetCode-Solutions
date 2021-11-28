c3a6f23474d18dea39b671b813c3ce94702d683d
Clark Long <halolong@hotmail.com>
2021-11-27 20:59:31 -0800

    2021-11-27-20:59:31 Changes were made in following file(s): 797.All Paths From Sourceto Target, 797.Target

--- Leetcode/src/com/leetcode/backtracking/_797_AllPathsFromSourcetoTarget.java
+++ Leetcode/src/com/leetcode/backtracking/_797_AllPathsFromSourcetoTarget.java
@@ -1,39 +1,36 @@
+/*
+ * @Date: 07/24/2020 17:59:37
+ * @LastEditTime: 11/27/2021 20:53:45
+ * @Description: Backtracking
+ */
 package com.leetcode.backtracking;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * @Date: 07/24/2020
- * @Description: backtracking
- **/
 public class _797_AllPathsFromSourcetoTarget {
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        // backtracking
         if (graph == null || graph.length == 0 || graph[0] == null || graph[0].length == 0) {
             return new ArrayList<>();
         }
-        int m = graph.length;
+
         List<List<Integer>> res = new ArrayList<>();
-        build(res, new ArrayList<>(), 0, graph);
+        build(graph, 0, res, new ArrayList<>());
         return res;
     }
 
-    private void build(List<List<Integer>> res, List<Integer> list, int curr, int[][] graph) {
-        if (curr == graph.length - 1) {
-            list.add(curr);
-            res.add(new ArrayList<>(list));
-            list.remove(list.size() - 1);
-            return;
+    private void build(int[][] graph, int current, List<List<Integer>> res, List<Integer> path) {
+        path.add(current);
+        if (current == graph.length - 1) {
+            res.add(new ArrayList<>(path));
         }
 
-        int[] adjs = graph[curr];
+        int[] adjs = graph[current];
         if (adjs != null) {
-            list.add(curr);
-            for (int next : adjs) {
-                build(res, list, next, graph);
+            for (int adj : adjs) {
+                build(graph, adj, res, path);
             }
-            list.remove(list.size() - 1);
         }
+        path.remove(path.size() - 1);
     }
 }
