72dbcdb3b57f88421c433efc3392bea548b97306
Xingyu Long <halolong@hotmail.com>
2022-08-31 14:32:42 -0700

    2022-08-31-14:32:42 Changes were made in following file(s): 1329.Sortthe Matrix Diagonally, 1329.

--- Leetcode/src/com/leetcode/matrix/_1329_SorttheMatrixDiagonally.java
+++ Leetcode/src/com/leetcode/matrix/_1329_SorttheMatrixDiagonally.java
@@ -13,59 +13,12 @@ import java.util.Map;
 import java.util.PriorityQueue;
 
 public class _1329_SorttheMatrixDiagonally {
-    // time: O(m + n) max log max(# of diag)
-    // space: O(m * n) clone
-    public int[][] diagonalSort(int[][] mat) {
-        if (mat == null || mat.length == 0 || mat[0] == null || mat[0].length == 0) return new int[][]{};
-        int m = mat.length;
-        int n = mat[0].length;
-        for (int j = n - 1; j >= 0; j--) {
-            int col = j;
-            int row = 0;
-            List<Integer> list = new LinkedList<>();
-            while (row < m && col < n) {
-                list.add(mat[row][col]);
-                row++;
-                col++;
-            }
-            Collections.sort(list);
-            col = j;
-            row = 0;
-            int k = 0;
-            while (row < m && col < n) {
-                mat[row][col] = list.get(k++);
-                row++;
-                col++;
-            }
-        }
-
-        for (int i = 1; i < m; i++) {
-            int col = 0;
-            int row = i;
-            List<Integer> list = new LinkedList<>();
-            while (row < m && col < n) {
-                list.add(mat[row][col]);
-                row++;
-                col++;
-            }
-            Collections.sort(list);
-            col = 0;
-            row = i;
-            int k = 0;
-            while (row < m && col < n) {
-                mat[row][col] = list.get(k++);
-                row++;
-                col++;
-            }
-        }
-        return mat;
-    }
 
     // Use PriorityQueue instead of sorting each time.
     public int[][] diagonalSort2(int[][] mat) {
         if (mat == null || mat.length == 0 ||
                 mat[0] == null || mat[0].length == 0)
-            return new int[][]{};
+            return new int[][] {};
         int m = mat.length;
         int n = mat[0].length;
         // use i - j to identify each diag.
