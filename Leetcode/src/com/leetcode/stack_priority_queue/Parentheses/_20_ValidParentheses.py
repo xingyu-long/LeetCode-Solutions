00522bb43bb6a67456f4e7a0f556fc70e09c386a
Xingyu Long <halolong@hotmail.com>
2024-04-09 18:48:18 -0700

    2024-04-09-18:48:18 Changes were made in following file(s): 20.Valid Parentheses

--- Leetcode/src/com/leetcode/stack_priority_queue/Parentheses/_20_ValidParentheses.py
+++ Leetcode/src/com/leetcode/stack_priority_queue/Parentheses/_20_ValidParentheses.py
@@ -1,43 +1,38 @@
-from Leetcode.src.com.leetcode.common import TreeNode
-
-
 class Solution:
     # 利用一样的思路，但可否用map优化？
     def isValid(self, s: str) -> bool:
         stack = []
         for ch in s:
-            if ch == '(' or ch == '[' or ch == '{':
+            if ch == "(" or ch == "[" or ch == "{":
                 stack.append(ch)
             else:
                 # If stack is empty
                 if not stack:
                     return False
-                if stack[-1] == '(' and ch != ')':
+                if stack[-1] == "(" and ch != ")":
                     return False
-                if stack[-1] == '[' and ch != ']':
+                if stack[-1] == "[" and ch != "]":
                     return False
-                if stack[-1] == '{' and ch != '}':
+                if stack[-1] == "{" and ch != "}":
                     return False
                 stack.pop()
 
         return len(stack) == 0
 
 
-class Solution_2:
+class Solution2:
     def isValid(self, s: str) -> bool:
+        m = {")": "(", "}": "{", "]": "["}
         stack = []
-        m = {']': '[', '}': '{', ')': '('}
         for ch in s:
             if ch in m.values():
                 stack.append(ch)
-            else:
-                # If stack is empty or doesn't match.
-                if not stack or stack[-1] != m[ch]:
+            elif ch in m:
+                if stack and m[ch] == stack[-1]:
+                    stack.pop()
+                else:
                     return False
-                stack.pop()
-
+            else:
+                # unreachable
+                return False
         return len(stack) == 0
-
-
-solution = Solution()
-print(solution.isValid('[]{}'))
