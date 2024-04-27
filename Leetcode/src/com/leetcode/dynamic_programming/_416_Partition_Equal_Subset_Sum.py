3ea7f037d8fef1b662a2189717944f5c0e8df4dd
Xingyu Long <halolong@hotmail.com>
2024-04-27 10:15:16 -0700

    2024-04-27-10:15:16 Changes were made in following file(s): 416.Sum, 300.Subsequence, 140., 139., 152., 322.

--- Leetcode/src/com/leetcode/dynamic_programming/_416_Partition_Equal_Subset_Sum.py
+++ Leetcode/src/com/leetcode/dynamic_programming/_416_Partition_Equal_Subset_Sum.py
@@ -1,28 +1,23 @@
-'''
-Date: 11/27/2020 11:48:56
-LastEditTime: 11/27/2020 11:49:21
-Description: 0/1 knapsack
-'''
+from typing import List
 
 
 class Solution:
+    # time: O(target * n)
+    # time: O(target * n)
     def canPartition(self, nums: List[int]) -> bool:
-        if not nums:
-            return False
         total = sum(nums)
         if total % 2 != 0:
             return False
-        num = len(nums)
+        n = len(nums)
         target = total // 2
-        dp = [[False for _ in range(target + 1)] for _ in range(num + 1)]
+        dp = [[False] * (target + 1) for _ in range(n + 1)]
         dp[0][0] = True
-        for i in range(1, num + 1):
+        for i in range(1, n + 1):
             dp[i][0] = True
-
-        for i in range(1, num + 1):
             for j in range(1, target + 1):
-                dp[i][j] = dp[i - 1][j]
-                if j >= nums[i - 1]:
-                    dp[i][j] = dp[i][j] or dp[i - 1][j - nums[i - 1]]
+                coin = nums[i - 1]
+                not_use = dp[i - 1][j]
+                use = dp[i - 1][j - coin] if j >= coin else False
+                dp[i][j] = not_use or use
 
-        return dp[num][target]
+        return dp[n][target]
--- Leetcode/src/com/leetcode/dynamic_programming/back_check_dp/_300_Longest_Increasing_Subsequence.py
+++ Leetcode/src/com/leetcode/dynamic_programming/back_check_dp/_300_Longest_Increasing_Subsequence.py
@@ -1,48 +1,53 @@
-'''
-Date: 12/07/2020 16:06:36
-LastEditTime: 12/07/2020 17:17:22
-Description: DP / Binary Search
-'''
+from typing import List
 
 
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        dp = [0 for _ in range(len(nums))]
+        n = len(nums)
+        dp = [0] * n
         res = 0
-        for i in range(0, len(nums)):
-            curr_max = 0
-            for j in range(0, i):
-                if nums[i] > nums[j]:
-                    curr_max = max(curr_max, dp[j])
-            dp[i] = curr_max + 1
+        for i in range(n):
+            best = 1
+            for j in range(i + 1):
+                if nums[j] < nums[i]:
+                    best = max(best, dp[j] + 1)
+            dp[i] = best
             res = max(res, dp[i])
         return res
 
 
-class Solution_2:
+class Solution2:
+    # time: O(nlogn)
+    # space: O(n)
     def lengthOfLIS(self, nums: List[int]) -> int:
-        sort_list = [0 for _ in range(len(nums))]
+        n = len(nums)
+        arr = [0] * n
         res = 0
-        sort_list[res] = nums[0]
-        res += 1
-        for num in nums:
-            if num > sort_list[res - 1]:
-                sort_list[res] = num
-                res += 1
-            else:
-                idx = self.find(sort_list, num, 0, res - 1)
-                sort_list[idx] = num
-        return res
 
-    def find(self, sort_list, target, left, right):
-        while left + 1 < right:
-            mid = left + (right - left) // 2
-            if sort_list[mid] >= target:
-                right = mid
+        def find_insert_pos(arr, target, l, r):
+            while l + 1 < r:
+                mid = l + (r - l) // 2
+                if arr[mid] > target:
+                    r = mid
+                else:
+                    l = mid
+            if target <= arr[l]:
+                return l
+            if target > arr[r]:
+                return r + 1
+            return r
+
+        for index, num in enumerate(nums):
+            if res > 0:
+                if num > arr[res - 1]:
+                    arr[res] = num
+                    res += 1
+                else:
+                    # find the place and insert
+                    correct_idx = find_insert_pos(arr, num, 0, res - 1)
+                    arr[correct_idx] = num
             else:
-                left = mid
-        if target <= sort_list[left]:
-            return left
-        if target > sort_list[right]:
-            return right + 1
-        return right
+                arr[res] = num
+                res += 1
+
+        return res
