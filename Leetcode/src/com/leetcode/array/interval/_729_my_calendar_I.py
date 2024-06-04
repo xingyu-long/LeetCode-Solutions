98ab43304dfcc9b52fa2ddebcc81c194c5362d5a
Xingyu Long <halolong@hotmail.com>
2024-06-04 08:25:55 -0700

    2024-06-04-08:25:55 Changes were made in following file(s): 729.I, 731., 732.

--- Leetcode/src/com/leetcode/array/interval/_729_my_calendar_I.py
+++ Leetcode/src/com/leetcode/array/interval/_729_my_calendar_I.py
@@ -1,3 +1,6 @@
+from sortedcontainers import SortedList
+
+
 class MyCalendar:
 
     def __init__(self):
@@ -16,6 +19,17 @@ class MyCalendar:
         return True
 
 
-# Your MyCalendar object will be instantiated and called as such:
-# obj = MyCalendar()
-# param_1 = obj.book(start,end)
+class MyCalendar:
+
+    def __init__(self):
+        self.data = SortedList(
+            [(float("-inf"), float("-inf")), (float("inf"), float("inf"))]
+        )
+
+    # time: O(logN)
+    def book(self, start: int, end: int) -> bool:
+        idx = self.data.bisect_right((start, end))
+        if start < self.data[idx - 1][1] or end > self.data[idx][0]:
+            return False
+        self.data.add((start, end))
+        return True
